b403eb860948c054ba65bc4a82d77c6ecd094854
CallMeNAKS <thefan51999@gmail.com>
2024-11-29 10:24:50 +0300

    add generic source

---
+++ Assets/_StickHero/CodeBase/Infrastructure/GenericSource/GenericSource.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Infrastructure.GenericSource
+{
+    public abstract class GenericSource<T> where T : MonoBehaviour
+    {
+        protected T _prefab { get; private set; }
+
+        public GenericSource(T prefab)
+        {
+            _prefab = prefab;
+        }
+        
+        public abstract T Get();
+    }
+}
---
+++ Assets/_StickHero/CodeBase/Infrastructure/GenericSource/ObjectPool/GenericObjectPool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure.GenericSource.ObjectPool
+{
+    public class GenericObjectPool<T> : GenericSource<T> where T : MonoBehaviour
+    {
+        private List<T> _objects = new List<T>();
+
+        public GenericObjectPool(T prefab) : base(prefab)
+        {
+        }
+
+        public override T Get()
+        {
+            if (_prefab == null)
+                throw new InvalidOperationException("Prefab is not set.");
+
+            var pooledObject = _objects.FirstOrDefault(obj => !obj.gameObject.activeSelf) ?? CreateNewObject();
+            return pooledObject;
+        }
+
+        private T CreateNewObject()
+        {
+            var newObject = GameObject.Instantiate(_prefab);
+            _objects.Add(newObject);
+            return newObject;
+        }
+
+    }
+}
