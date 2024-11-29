9b93c973b62e0f5f5e56e84410dfba94514e2448
CallMeNAKS <thefan51999@gmail.com>
2024-11-29 17:48:07 +0300

    add platform builder

---
+++ Assets/_StickHero/CodeBase/Platform/PlatformBuilder.cs
@@ -0,0 +1,48 @@
+﻿using CodeBase.Infrastructure.GenericSource;
+using UnityEngine;
+
+namespace CodeBase.Platform
+{
+    public class PlatformBuilder
+    {
+        private Transform _currentPlatformTransform;
+        private readonly GenericSource<Platform> _platformSource;
+        private readonly PlatformConfig _platformConfig;
+
+        public PlatformBuilder(Transform currentPlatformTransform,
+            GenericSource<Platform> platformSource,
+            PlatformConfig platformConfig)
+        {
+            _currentPlatformTransform = currentPlatformTransform;
+            _platformSource = platformSource;
+            _platformConfig = platformConfig;
+        }
+
+        public void Build()
+        {
+            var size = Random.Range(_platformConfig.MinSize, _platformConfig.MaxSize);
+            var range = Random.Range(_platformConfig.MinRange, _platformConfig.MaxRange);
+            var placeForPlatform = CalculatePlatformPosition(size, range);
+
+            var newPlatform = CreatePlatform(placeForPlatform, size);
+            
+            _currentPlatformTransform = newPlatform.transform;
+        }
+
+        private Vector3 CalculatePlatformPosition(float platformSize, float range)
+        {
+            var currentPlatformWidth = _currentPlatformTransform.localScale.x;
+            return _currentPlatformTransform.position + 
+                   new Vector3(currentPlatformWidth / 2 + range + platformSize / 2, 0, 0);
+        }
+
+        private Platform CreatePlatform(Vector3 position, float platformSize)
+        {
+            var newPlatform = _platformSource.Get();
+            newPlatform.transform.position = position;
+            newPlatform.PlatformTransform.localScale = 
+                new Vector2(platformSize, newPlatform.PlatformTransform.localScale.y);
+            return newPlatform;
+        }
+    }
+}
---
+++ Assets/_StickHero/CodeBase/Platform/PlatformConfig.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Platform
+{
+    [CreateAssetMenu(menuName = "Platform", fileName = "PlatformConfig", order = 0)]
+    public class PlatformConfig : ScriptableObject
+    {
+        [SerializeField] private float _minSize;
+        [SerializeField] private float _maxSize;
+        [SerializeField] private float _minRange;
+        [SerializeField] private float _maxRange;
+        
+        
+        public float MinSize => _minSize;
+        public float MaxSize => _maxSize;
+        public float MinRange => _minRange;
+        public float MaxRange => _maxRange;
+    }
+}
