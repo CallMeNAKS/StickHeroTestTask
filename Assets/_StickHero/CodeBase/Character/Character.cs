e4105532a364701c1fd4d977c20684f5a4ebce20
CallMeNAKS <thefan51999@gmail.com>
2024-12-01 22:48:39 +0300

    add character and character mover

---
+++ Assets/_StickHero/CodeBase/Character/Character.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Character
+{
+    public class Character : MonoBehaviour
+    {
+        [SerializeField] private Transform _bridgePoint;
+        
+        public Transform BridgePoint => _bridgePoint;
+    }
+}
---
+++ Assets/_StickHero/CodeBase/Character/CharacterMover.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using CodeBase.Infrastructure.CoroutineService;
+using DG.Tweening;
+using UnityEngine;
+
+namespace CodeBase.Character
+{
+    public class CharacterMover
+    {
+        private readonly Transform _characterTransform;
+        private readonly Coroutines _coroutines;
+        private readonly CharacterConfig _characterConfig;
+        
+        private readonly Vector3 _fallPosition = Vector3.down * 10;
+
+        public CharacterMover(Transform characterTransform, Coroutines coroutines, CharacterConfig characterConfig)
+        {
+            _characterTransform = characterTransform;
+            _coroutines = coroutines;
+            _characterConfig = characterConfig;
+        }
+
+        public IEnumerator Move(Vector3 endPosition)
+        {
+            yield return _coroutines.StartCoroutine(MoveCoroutine(endPosition));
+        }
+
+        public void Fall()
+        {
+            var endPosition = _characterTransform.position + _fallPosition;
+
+            _characterTransform.DOMove(endPosition, _characterConfig.FallAnimationDuration);
+        }
+
+        private IEnumerator MoveCoroutine(Vector3 endPosition)
+        {
+            while (Vector3.Distance(_characterTransform.position, endPosition) > 0.01f)
+            {
+                _characterTransform.position = Vector3.MoveTowards(
+                    _characterTransform.position,
+                    endPosition,
+                    _characterConfig.CharacterSpeed * Time.deltaTime);
+
+                yield return null;
+            }
+
+            _characterTransform.position = endPosition;
+        }
+    }
+}
