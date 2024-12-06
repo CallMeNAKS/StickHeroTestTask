e5e20e30a161b552a26cf830b6ae685a55b105ac
CallMeNAKS <thefan51999@gmail.com>
2024-12-06 16:06:24 +0300

    add character animator

---
+++ Assets/_StickHero/CodeBase/Character/CharacterAnimation/Animator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeBase.Character.CharacterAnimation
+{
+    public enum CharacterAnimationNames
+    {
+        Idle,
+        Walk,
+        ActiveWait
+    }
+    
+    public class Animator : MonoBehaviour
+    {
+        [SerializeField] private GameObject _walkAnimation;
+        [SerializeField] private GameObject _activeWaitAnimation;
+        [SerializeField] private GameObject _waitAnimation;
+
+        private readonly Dictionary<CharacterAnimationNames, GameObject> _animations = new();
+        private CharacterAnimationNames _currentAnimation = CharacterAnimationNames.Idle;
+        
+        private void Awake()
+        {
+            _animations.Add(CharacterAnimationNames.Walk, _walkAnimation);
+            _animations.Add(CharacterAnimationNames.ActiveWait, _activeWaitAnimation);
+            _animations.Add(CharacterAnimationNames.Idle, _waitAnimation);
+            
+            PlayAnimation(CharacterAnimationNames.Idle);
+        }
+
+        public void PlayAnimation(CharacterAnimationNames animationName)
+        {
+            if (animationName == _currentAnimation) return;
+            
+            _animations[_currentAnimation].gameObject.SetActive(false);
+            _animations[animationName].gameObject.SetActive(true);
+            _currentAnimation = animationName;
+        }
+    }
+}
