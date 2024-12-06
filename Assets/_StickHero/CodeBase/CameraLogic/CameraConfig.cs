14d644abbf5b3ee3a00cca9b711b393e9258f900
CallMeNAKS <thefan51999@gmail.com>
2024-12-06 16:00:33 +0300

    upd camera logic

--- Assets/_StickHero/CodeBase/CameraLogic/CameraConfig.cs
+++ Assets/_StickHero/CodeBase/CameraLogic/CameraConfig.cs
@@ -4,6 +4,10 @@
 public class CameraConfig : ScriptableObject
 {
     [SerializeField] private float _moveDuration;
+    [SerializeField] private float _cameraSize = 5f;
+    [SerializeField] private float _cameraReSizeDuration = 1f;
     
     public float MoveDuration => _moveDuration;
+    public float CameraSize => _cameraSize;
+    public float CameraReSizeDuration => _cameraReSizeDuration;
 }
--- Assets/_StickHero/CodeBase/CameraLogic/CameraMover.cs
+++ Assets/_StickHero/CodeBase/CameraLogic/CameraMover.cs
@@ -1,4 +1,5 @@
-﻿using CodeBase.Platform;
+﻿using System.Collections;
+using CodeBase.Platform;
 using DG.Tweening;
 using UnityEngine;
 
@@ -6,23 +7,37 @@ public class CameraMover
 {
     private readonly Camera _camera;
     private readonly CameraConfig _cameraConfig;
+    private readonly Vector3 _startPosition;
+
+    private float _startCameraSize;
 
     public CameraMover(Camera camera, CameraConfig cameraConfig)
     {
         _camera = camera;    
         _cameraConfig = cameraConfig;
+        _startPosition = camera.transform.position;
     }
     
     public void MoveToPlatform(Platform currentPlatform)
     {
-        var platformRenderer = currentPlatform.gameObject.GetComponent<SpriteRenderer>();
-        
-        float platformLeftEdge = platformRenderer.bounds.min.x;
+        float platformLeftEdge = currentPlatform.MinX;
 
-        float cameraHalfWidth = _camera.orthographicSize * _camera.aspect;
+        float cameraHalfWidth = _cameraConfig.CameraSize * _camera.aspect;
 
         Vector3 newCameraPosition = _camera.transform.position;
         newCameraPosition.x = platformLeftEdge + cameraHalfWidth;
         _camera.transform.DOMove(newCameraPosition, _cameraConfig.MoveDuration);
+    }
+
+    public IEnumerator ScaleCamera()
+    {
+        _startCameraSize = _camera.orthographicSize;
+        yield return _camera.DOOrthoSize(_cameraConfig.CameraSize, _cameraConfig.CameraReSizeDuration).WaitForCompletion();
+    }
+
+    public void ResetCamera()
+    {
+        _camera.transform.position = _startPosition;
+        _camera.orthographicSize = _startCameraSize;
     }
 }
