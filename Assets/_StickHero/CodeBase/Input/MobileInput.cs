b708e5dd05dfbd363cc10f27197118339af05775
CallMeNAKS <thefan51999@gmail.com>
2024-12-06 16:06:13 +0300

    add mobile input

---
+++ Assets/_StickHero/CodeBase/Input/MobileInput.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace CodeBase.Input
+{
+    public class MobileInput : MonoBehaviour, IInputHandler
+    {
+        public event Action Pressed;
+        public event Action EndPressing;
+        public event Action Clicked;
+        
+        private bool _isPressed = false;
+
+        private void Update()
+        {
+            if (UnityEngine.Input.touchCount > 0)
+            {
+                Touch touch = UnityEngine.Input.GetTouch(0);
+
+                if (touch.phase == TouchPhase.Began)
+                {
+                    _isPressed = true;
+                    Clicked?.Invoke();
+                }
+
+                if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+                {
+                    _isPressed = false;
+                    EndPressing?.Invoke();
+                }
+            }
+
+            if (_isPressed)
+            {
+                Pressed?.Invoke();
+            }
+        }
+    }
+}
