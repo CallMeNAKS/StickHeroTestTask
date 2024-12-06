9b821045138e3bbb746a2388ec1c08ac1a4f25d0
CallMeNAKS <thefan51999@gmail.com>
2024-12-06 15:58:09 +0300

    add ui logic

---
+++ Assets/_StickHero/CodeBase/Infrastructure/Installers/UIInstaller.cs
@@ -0,0 +1,48 @@
+﻿using CodeBase.UI;
+using UnityEngine;
+using Zenject;
+
+namespace CodeBase.Infrastructure.Installers
+{
+    public class UIInstaller : MonoInstaller
+    {
+        [Header("UI Prefabs")]
+        [SerializeField] private StartView _startView;
+        [SerializeField] private PlayView _playView;
+        [SerializeField] private LoseView _loseView;
+
+        public override void InstallBindings()
+        {
+            BindStartView();
+            BindPlayView();
+            BindLoseView();
+        }
+
+        private void BindLoseView()
+        {
+            Container.Bind<LoseView>()
+                .FromComponentInNewPrefab(_loseView)
+                .WithGameObjectName("[LoseView]")
+                .AsSingle()
+                .NonLazy();
+        }
+
+        private void BindPlayView()
+        {
+            Container.Bind<PlayView>()
+                .FromComponentInNewPrefab(_playView)
+                .WithGameObjectName("[PlayView]")
+                .AsSingle()
+                .NonLazy();
+        }
+
+        private void BindStartView()
+        {
+            Container.Bind<StartView>()
+                .FromComponentInNewPrefab(_startView)
+                .WithGameObjectName("[StartView]")
+                .AsSingle()
+                .NonLazy();
+        }
+    }
+}
---
+++ Assets/_StickHero/CodeBase/UI/UIView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.UI
+{
+    public abstract class UIView : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _uiElements;
+
+        private void Awake()
+        {
+            foreach (var uiElement in _uiElements)
+            {
+                uiElement.SetActive(false);
+            }
+        }
+
+        public void ShowUI()
+        {
+            ToggleUI(true);
+        }
+
+        public void HideUI()
+        {
+            ToggleUI(false);
+        }
+
+        private void ToggleUI(bool isEnable)
+        {
+            foreach (GameObject uiElement in _uiElements)
+            {
+                uiElement.SetActive(isEnable);
+            }
+        }
+    }
+}
