c0d8977982b7d96d0ad03ddb56a3614e7bc2cc3e
CallMeNAKS <thefan51999@gmail.com>
2024-12-06 16:05:41 +0300

    add scoring

---
+++ Assets/_StickHero/CodeBase/Score/Scoring.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CodeBase.Score
+{
+    public class Scoring
+    {
+        private int _score = 0;
+        
+        public event Action<int> ScoreChanged;
+
+        public void AddScore(int score)
+        {
+            if (score > 0)
+            {
+                _score += score;
+                ScoreChanged?.Invoke(_score);
+            }
+        }
+
+        public void ResetScore()
+        {
+            _score = 0;
+            ScoreChanged?.Invoke(_score);
+        }
+    }
+}
