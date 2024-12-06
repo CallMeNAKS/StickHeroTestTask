296efd7d50a68c8239f3053061d8b7aa727bd331
CallMeNAKS <thefan51999@gmail.com>
2024-12-06 16:05:59 +0300

    add reset logic

---
+++ Assets/_StickHero/CodeBase/Infrastructure/ResetLogic/Recycling.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure.ResetLogic
+{
+    public class Recycling
+    {
+        private List<GameObject> _itemsForRecycling = new List<GameObject>();
+
+        public void AddItemToRecycling(GameObject item)
+        {
+            _itemsForRecycling.Add(item);
+        }
+
+        public void Clear()
+        {
+            foreach (var gameObject in _itemsForRecycling)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
